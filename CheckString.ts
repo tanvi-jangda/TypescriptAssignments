6083074c9a5ea6b0df21134daad253f669e04fbc
tanvi-jangda <58063235+tanvi-jangda@users.noreply.github.com>
2023-03-25 18:43:19 +0530

    Delete CheckString.ts
    not required

--- CheckString.ts
+++
@@ -1,5 +0,0 @@
-function ChkString(myString:string)
-{
-myString.indexOf('Marvellous') != -1? console.log("String contains Marvellous in it.") :  console.log("String does not contain Marvellous in it.")
-}
-ChkString("Pune Kothrud Marvellous Infosystems");
